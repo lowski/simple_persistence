2e1ece2c6b7048c9ec4fdd8fef36323b9573074a
Leonard von Lojewski <dev@lojewski.dev>
2023-09-10 12:02:04 +0200

    fix: export kv store

--- lib/simple_persistence.dart
+++ lib/simple_persistence.dart
@@ -4,4 +4,5 @@ export 'src/storable.dart';
 export 'src/storable_factory.dart';
 export 'src/stores/big_json_store.dart';
 export 'src/stores/json_file_store.dart';
+export 'src/stores/kv_store.dart';
 export 'src/stores/store.dart';
